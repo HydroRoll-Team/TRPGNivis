ba4129933cdb6d91e695b2de900b8753652ec385
简律纯 <i@jyunko.cn>
2023-09-27 17:31:16 +0800

    feat(pyproject): 优化python包管理结构

---
+++ src/psi/__init__.py
@@ -0,0 +1,26 @@
+"""Psi
+@TODO 词法分析器
+@BODY 似乎要写的还蛮多的，所以先写几个TODO List
+"""
+
+__all__ = ['psi']
+
+from psi.execution import Execution
+
+class Psi:
+    def __init__(self, input):
+        self.input = input
+        self.execution = Execution(input)
+        self.result = None
+
+    def execute(self):
+        self.result = self.execution.execute()
+        return self.result
+
+    def get_result(self):
+        return self.result
+
+    def set_input(self, input):
+        self.input = input
+        self.execution = Execution(input)
+        self.result = None
